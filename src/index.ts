278ebec8219622ef1fc11074f354ea726391a093
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-07-31 17:53:44 +0200

    prod: removed animated grain try

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-// Importieren der Funktionen und Typen
+
 import { Gradient } from "./gradients/Gradient";
 import { applyGrain } from "./effects/applyGrain";
-import { applyAnimatedGrain } from "./effects/applyAnimatedGrain";
-import { applyAnimatedGrainEffect } from "./effects/applyAnimatedGrainEffect";
 import { GradientOptions } from "./types/GradientOptions";
 
-// Exportieren der Funktionen und Typen
 export {
   Gradient,
   applyGrain,
-  applyAnimatedGrain,
-  applyAnimatedGrainEffect,
   GradientOptions,
 };
